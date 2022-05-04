6b1815c79b6c16b523a188d8a79b736f4a0a9ea5
rickymsmith <35548840+rickymsmith@users.noreply.github.com>
2022-05-03 21:09:22 -0500

    weather bot bot.js cleanup

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,8 @@
 import * as Discord from 'discord.js';
 import { getTemperature } from './weather.js';
 
+let regExp = ""
+
 const client = new Discord.Client(
     {
         intents: ['GUILDS', 'GUILD_MESSAGES'],
@@ -13,17 +15,16 @@ const token = "";
  * @param {discord.Message} message 
  */
 async function onMessage(message){
-    if (ZipCodeFunc(message.content)) {
+    if (zipCodeFunc(message.content)) {
         const temp = await getTemperature(message.content)
         message.reply(`It is currently ${temp} outside`)
     };
 }
 
-function ZipCodeFunc(str)
-{
- RegExp = /^[0-9]{5}/;
+function zipCodeFunc(str){
+ regExp = /^[0-9]{5}/;
   
-        if (RegExp.test(str))
+        if (regExp.test(str))
           {
             return true;
           }
@@ -36,6 +37,5 @@ function ZipCodeFunc(str)
 
 client.on('messageCreate', onMessage);
 
-console.log(ZipCodeFunc(64109))
 
 client.login(token);
